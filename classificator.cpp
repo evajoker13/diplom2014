c4b723889b6bbd8c200536976f621ad984e339fb
Ivanka Bahniuk <evajoker13@gmail.com>
2014-06-20 20:43:02 +0300

    classificator: re-organize training

--- classificator.cpp
+++ classificator.cpp
@@ -1,15 +1,19 @@
 #include "classificator.hpp"
-#include "transform.hpp"
+#include "vec.hpp"
 #include <iostream>
 #include <vector>
 #include <array>
 
 using namespace std;
 
+namespace {
+
+constexpr size_t chars_count = std::tuple_size<properties_type>::value;
+
 template <typename I1, typename I2>
-std::vector<std::pair<double, double>> zip(I1 first1, I1 last1, I2 first2)
+std::vector<vec<double, 2>> zip(I1 first1, I1 last1, I2 first2)
 {
-    std::vector<std::pair<double, double>> result;
+    std::vector<vec<double, 2>> result;
     result.reserve(last1 - first1);
     auto i1 = first1;
     auto i2 = first2;
@@ -17,59 +21,71 @@ std::vector<std::pair<double, double>> zip(I1 first1, I1 last1, I2 first2)
     return result;
 }
 
+struct distances
+{
+    array<vector<double>, chars_count>  aa, ab, ba, bb;
+
+    distances(const psamples_type &a, const psamples_type &b)
+    {
+        const size_t n = a.size(), m = b.size();
+
+        // build distance per characterestic
+        for (size_t k = 0; k < chars_count; ++k)
+        {
+            aa[k].reserve(n);
+            ab[k].reserve(n);
+            for (auto i = a.begin(); i != a.end(); ++i)
+            {
+                double p = 0;
+                for (auto j = a.begin(); j != a.end(); ++j)
+                {
+                    if (i == j)
+                        continue;
+                    p += (*i)[k].distance((*j)[k]);
+                }
+                aa[k].emplace_back(p / (n - 1));
+
+                double q = 0;
+                for (auto j = b.begin(); j != b.end(); ++j)
+                {
+                    q += (*i)[k].distance((*j)[k]);
+                }
+                ab[k].emplace_back(q / m);
+                //cout << "k = " << k <<  ", aa = " << aa[k].back() << ", ab = " << ab[k].back() << endl;
+            }
+
+            ba[k].reserve(m);
+            bb[k].reserve(m);
+            for (auto i = a.begin(); i != a.end(); ++i)
+            {
+                double p = 0;
+                for (auto j = a.begin(); j != a.end(); ++j)
+                {
+                    p += (*i)[k].distance((*j)[k]);
+                }
+                ba[k].emplace_back(p / n);
+
+                double q = 0;
+                for (auto j = b.begin(); j != b.end(); ++j)
+                {
+                    if (i == j)
+                        continue;
+                    q += (*i)[k].distance((*j)[k]);
+                }
+                bb[k].emplace_back(q / (m - 1));
+                //cout << "k = " << k <<  ", ba = " << ba[k].back() << ", bb = " << bb[k].back() << endl;
+            }
+        }
+    }
+};
+
+}
+
 void classificator::train(const psamples_type &a, const psamples_type &b)
 {
     const size_t n = a.size(), m = b.size();
 
-    array<vector<double>, chars_count>  aa, ab, ba, bb;
-
-    // build distance per characterestic
-    for (size_t k = 0; k < chars_count; ++k)
-    {
-        aa[k].reserve(n);
-        ab[k].reserve(n);
-        for (auto i = a.begin(); i != a.end(); ++i)
-        {
-            double p = 0;
-            for (auto j = a.begin(); j != a.end(); ++j)
-            {
-                if (i == j)
-                    continue;
-                p += (*i)[k].distance((*j)[k]);
-            }
-            aa[k].emplace_back(p / (n - 1));
-
-            double q = 0;
-            for (auto j = b.begin(); j != b.end(); ++j)
-            {
-                q += (*i)[k].distance((*j)[k]);
-            }
-            ab[k].emplace_back(q / m);
-            //cout << "k = " << k <<  ", aa = " << aa[k].back() << ", ab = " << ab[k].back() << endl;
-        }
-
-        ba[k].reserve(m);
-        bb[k].reserve(m);
-        for (auto i = a.begin(); i != a.end(); ++i)
-        {
-            double p = 0;
-            for (auto j = a.begin(); j != a.end(); ++j)
-            {
-                p += (*i)[k].distance((*j)[k]);
-            }
-            ba[k].emplace_back(p / n);
-
-            double q = 0;
-            for (auto j = b.begin(); j != b.end(); ++j)
-            {
-                if (i == j)
-                    continue;
-                q += (*i)[k].distance((*j)[k]);
-            }
-            bb[k].emplace_back(q / (m - 1));
-            //cout << "k = " << k <<  ", ba = " << ba[k].back() << ", bb = " << bb[k].back() << endl;
-        }
-    }
+    distances distances(a, b);
 
     // couple together characteristics
     for (size_t t = 0; t < chars_count; ++t)
@@ -77,8 +93,7 @@ void classificator::train(const psamples_type &a, const psamples_type &b)
         for (size_t s = 0; s < chars_count; ++s)
         {
             // TODO: ellipsis aa, ab, ba, bb
-            auto points = zip(begin(aa[t]), end(aa[t]), begin(aa[s]));
-            auto tf = tmatrix<double>::translate({1, 1});
+            auto points = zip(begin(distances.aa[t]), end(distances.aa[t]), begin(distances.aa[s]));
         }
     }
 }
--- classificator.hpp
+++ classificator.hpp
@@ -4,7 +4,6 @@
 
 class classificator
 {
-    static constexpr size_t chars_count = std::tuple_size<properties_type>::value;
 public:
     void train(const psamples_type &a, const psamples_type &b);
 };
